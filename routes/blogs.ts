d27490ad45e80393dc44ace1a906715cd22ae892
E-V-Castillo <purelyforcode@gmail.com>
2023-01-14 19:04:45 +0800

    Got the API connected to the controller functions

--- routes/blogs.ts
+++ routes/blogs.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 import express,{NextFunction, Request, Response} from "express"
+import { nextTick } from "process";
 const router = express.Router()
 const prisma = new PrismaClient()
 
-async function createUser(data :{
+async function createBlog(data :{
         title: string,
         description: string
         markdown: string
@@ -18,22 +19,75 @@ async function createUser(data :{
 }
 
 
-router.get("/",(req : Request, res: Response) => {
-    console.log("something");
-})
-router.get("/:id",(req : Request, res: Response) =>{
-    console.log("something");
+async function getBlogs(){
+    try {
+        return await prisma.blog.findMany()
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getBlogById (id: number){
+    try {
+        return await prisma.blog.findMany({
+            where:{
+                id: id
+        }})
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+async function updateBlog (id:number, title: string, description: string,markdown: string){
+    const updatedBlog = prisma.blog.update({
+        where:{ id },
+        data:{
+            title: title,
+            description: description,
+            markdown: markdown
+        }
+    })
+    return updatedBlog
+}
+
+async function deleteBlog (id:number){
+    await prisma.blog.delete({
+        where: {id}
+    })
+}
+
+router.get("/", async (req : Request, res: Response, next: NextFunction) => {
+    try {
+        const blogs = await getBlogs()
+        res.json(blogs)
+        ;
+    } catch (error) {
+        next(error)
+    }
 })
 
-router.post("/", async (req : Request, res: Response, next: NextFunction) =>{
-    const data = {
-        title: "This is my first Blog",
-        description: "Welcome to my first Blog, this is my description",
-        markdown: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis sed dui et sollicitudin. Quisque dapibus non justo sed consectetur. Cras egestas, turpis et lobortis malesuada, quam lacus finibus enim, vitae aliquet magna lorem ut nulla. Fusce eu leo id arcu imperdiet hendrerit. Duis interdum quis dui non suscipit.",
-    }
-    console.log(data)
+
+router.get("/:id", async(req : Request, res: Response, next: NextFunction) =>{
     try {
-        createUser(data)
+        const {id} = req.params
+        const blog = await getBlogById(Number(id))
+        res.json(blog)
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+router.post("/", async (req : Request, res: Response, next: NextFunction) =>{
+    // const data = {
+    //     title: "This is my first Blog",
+    //     description: "Welcome to my first Blog, this is my description",
+    //     markdown: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis sed dui et sollicitudin. Quisque dapibus non justo sed consectetur. Cras egestas, turpis et lobortis malesuada, quam lacus finibus enim, vitae aliquet magna lorem ut nulla. Fusce eu leo id arcu imperdiet hendrerit. Duis interdum quis dui non suscipit.",
+    // }
+    // console.log(data)
+    try {
+        createBlog(req.body)
         res.status(201).send("User Created")
     } catch (error) {
         next(error)
@@ -41,12 +95,37 @@ router.post("/", async (req : Request, res: Response, next: NextFunction) =>{
     
 })
 
-router.patch("/:id",(req : Request, res: Response) =>{
-    console.log("something");
-})
+router.patch("/:id",(req : Request, res: Response, next: NextFunction) =>{
+    // const { id } = req.params
+    // const { title, description, markdown } = req.body
 
-router.delete("/:id",(req : Request, res: Response) =>{
-    console.log("something");
+    // if (!title|| !description|| !markdown){
+    //     res.status(400).send("Invalid form data")
+    //     return
+    // }else{
+    const {id} = req.params
+    const title = "Updated title"
+    const description = "Updated description"
+    const markdown = "Updated markdown"
+    try {
+        updateBlog(Number(id), title, description, markdown)
+        res.send(`Blog with id ${id} updated`)
+    } catch (error) {
+        next(error)
+    }
+    }
+/* }*/)
+
+router.delete("/:id",(req : Request, res: Response, next:NextFunction) =>{
+    try {
+        const {id} =  req.params
+        console.log(id)
+        // const id = 1
+        deleteBlog(Number(id))
+        res.send("deleted")
+    } catch (error) {
+        next(error)
+    }
 })
 
 export default router
--- server.ts
+++ server.ts
@@ -3,6 +3,8 @@ import blogsRoute from "./routes/blogs"
 //imports
 
 const app = express()
+
+app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 
 app.use("/blogs", blogsRoute)
